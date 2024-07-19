1d59ceabaa1439f74924823a92df8fdfa33fc3fc
pavel-a-borisov <pavel.a.borisov@gmail.com>
2024-07-19 17:14:35 +0300

    Remove ignored files and update .gitignore

--- scheduler.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS scheduler (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date CHAR(8) NOT NULL DEFAULT "",
-    title VARCHAR(128) NOT NULL DEFAULT "",
-    comment TEXT NOT NULL DEFAULT "",
-    repeat VARCHAR(128) NOT NULL DEFAULT ""
-);
-CREATE INDEX IF NOT EXISTS scheduler_date ON scheduler(date)
